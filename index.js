d8df10b1766ffbae6b692c1a7312ecd87eed8dca
aristoteles <aristotelesbr@gmail.com>
2018-07-22 11:48:24 -0300

    added first window

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const electron = require('electron');
 
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
+	const mainWindown = new BrowserWindow({});
+
+	mainWindown.loadURL(`file://${__dirname}/index.html`)
 })
