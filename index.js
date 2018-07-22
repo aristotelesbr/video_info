4185fe6f4855bdafabd4044e43222f1a8fefdc07
aristoteles <aristotelesbr@gmail.com>
2018-07-22 14:29:22 -0300

    addind ffmpeg integration and added ipc module to send informations

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const electron = require('electron');
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
 	const mainWindown = new BrowserWindow({});
 
 	mainWindown.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('video:submit', (event, path) => {
+	ffmpeg.ffprobe(path, (err, metadata) => {
+		console.log('Video duration is: ', metadata.format.duration)
+	})
 })
