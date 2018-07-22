a53c400c91d8accc99fb82e62bf78c2bc7675451
aristoteles <aristotelesbr@gmail.com>
2018-07-22 16:44:40 -0300

    added render metadata for duration of video

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const ffmpeg = require("fluent-ffmpeg");
 
 const { app, BrowserWindow, ipcMain } = electron;
 
-app.on('ready', () => {
-	const mainWindown = new BrowserWindow({});
+let mainWindown;
 
+app.on('ready', () => {
+	mainWindown = new BrowserWindow({});
 	mainWindown.loadURL(`file://${__dirname}/index.html`)
 })
 
 ipcMain.on('video:submit', (event, path) => {
 	ffmpeg.ffprobe(path, (err, metadata) => {
-		console.log('Video duration is: ', metadata.format.duration)
+		mainWindown.webContents.send('video:metadata', metadata.format.duration);
 	})
 })
